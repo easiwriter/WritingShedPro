3d163eec1d0bb18ef1fc0b6d20c44ad5284ec33b
Keith Lander <keithlander@me.com>
2025-12-01 11:01:43 +0000

    Revert: Remove invalid production parameter from cloudKitDatabase

--- WrtingShedPro/Writing Shed Pro/Write_App.swift
+++ WrtingShedPro/Writing Shed Pro/Write_App.swift
@@ -42,7 +42,7 @@ struct Write_App: App {
             "WritingShedProConfiguration",
             schema: schema,
             url: storeURL,
-            cloudKitDatabase: .private("iCloud.com.appworks.writingshedpro", production: true)
+            cloudKitDatabase: .private("iCloud.com.appworks.writingshedpro")
         )
 
         do {
