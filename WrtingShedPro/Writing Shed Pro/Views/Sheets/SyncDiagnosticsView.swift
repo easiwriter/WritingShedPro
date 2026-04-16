f25f568b376eef498e6925c9233610723008fb10
Keith Lander <keithlander@me.com>
2026-04-16 10:04:13 +0100

    Call checkForOrphanedPublications on appear in SyncDiagnosticsView

--- WrtingShedPro/Writing Shed Pro/Views/Sheets/SyncDiagnosticsView.swift
+++ WrtingShedPro/Writing Shed Pro/Views/Sheets/SyncDiagnosticsView.swift
@@ -69,6 +69,7 @@ struct SyncDiagnosticsView: View {
                 checkProjectOrderHealth()
                 checkCloudKitSubscriptions()
                 checkForOrphans()
+                checkForOrphanedPublications()
                 checkForDuplicateStyleSheets()
                 tombstoneCount = DeduplicationService.tombstoneCount
             }
